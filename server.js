71f3f4ca1eaf00593f7503567486ca90d95ecd79
Mihail Shumilov <mschumilow@gmail.com>
2015-04-09 12:07:36 +0300

    base server configuration

--- server.js
+++ server.js
@@ -10,3 +10,16 @@ var morgan          = require( 'morgan' );
 var bodyParser      = require( 'body-parser' );
 var methodOverride  = require( 'method-override' );
 
+
+mongoose.connect('mongodb://localhost:27017/todo');
+
+app.use(express.static(__dirname + '/web'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+
+app.listen(8080);
+console.log("App listening on port 8080");
