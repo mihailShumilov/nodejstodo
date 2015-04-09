880745b66ca8d640ab79302b3b636e3583de8ccc
Mihail Shumilov <mschumilow@gmail.com>
2015-04-09 11:48:35 +0300

    require libs

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 /**
  * Created by godson on 4/9/15.
  */
+
+
+var express         = require( 'express' );
+var app             = express();
+var mongoose        = require( 'mongoose' );
+var morgan          = require( 'morgan' );
+var bodyParser      = require( 'body-parser' );
+var methodOverride  = require( 'method-override' );
+
